09e678fc6fa0234bfdc41eab6fc180f351773dfa
Robbivan <96996213+robbivan@users.noreply.github.com>
2023-01-10 16:43:58 +0300

    Add references and header

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <algorithm> //sort
+#include <vector>
+#include "test.h"
 
 
 // Функция компаратор по умолчанию
@@ -14,7 +16,7 @@ bool cmp(int a, int b){
 
 // Функция сортировки массива из библиотеки algorithm
 void def_sort(){
-    using std::cout;
+
     int a[5]={3,7,1,8,9};
     // указываются начало и конец
     std::sort(a,a+5,cmp); // тут есть 3 параметр компаратор
@@ -24,9 +26,28 @@ void def_sort(){
     }
 }
 
+void vector_work(){
+    using std::vector;
+    vector<int> v = {1,2,3,5,6};
+    vector <int> vv = v; //дилемма (vv это другой вектор)?
+    // в Java,Python это будут ссылки
+    // в Плюсах это КОПИЯ!
+    // чтобы была ссылка
+    vv[0]=0;
+    vector <int> &vvv =v;
+    vvv[0] =987;
+    cout<<vv[0]<<" Новый контейнер\n";
+    cout<<vvv[0]<<" Ссылка на v контейнер\n";
+    cout<<v[0]<<" Тут не меняли, но из-за vvv получилось = 987\n";
+
+
+}
+
 
 int main() {
+    using std::cout;
     def_sort();
-
+    Hello();
+    vector_work();
     return 0;
 }
---
+++ test.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+using std::cout;
+void Hello(){
+    cout<<"HELLO\n";
+}
