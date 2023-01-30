a8b13f2befabaf3462969b46d189eb3c83532916
Robbivan <96996213+robbivan@users.noreply.github.com>
2023-01-30 21:23:33 +0300

    Add casts to inheritance

--- OOP2.h
+++ OOP2.h
@@ -3,11 +3,25 @@ class Base{
 };
 
 
-class Derived: public Base{
+class Derived: public Base{ // private, то запретить cast от родителя
 
 };
 
 void on_oop_2(){
     Derived d;
     Base& b = d;
+
+    // slicing (срезка)
+    Base bb = d; // копируется часть Base из d типа Derived
+
+    Base& g = static_cast<Base&>(d); // явный каст, новая копия из derived
+    // если приватное наследование, то нельзя
+//    static_cast<Derived&>(b); // nак можно кастовать и вниз
+    // но нужно быть уверенным в том, что там Base, а не Derived
+    // static_cast может только вверх и вниз
+
+    // reinterpret_cast может также вправо и влево (кроме вверх и вниз)
+
+
+
 }
