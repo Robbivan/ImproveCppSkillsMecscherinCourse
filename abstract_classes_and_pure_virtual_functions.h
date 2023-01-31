7cba8453a15c1e10eb93b34752e332551fed5172
Robbivan <96996213+robbivan@users.noreply.github.com>
2023-01-31 18:34:05 +0300

    Add adstract class and pure func

---
+++ abstract_classes_and_pure_virtual_functions.h
@@ -0,0 +1,19 @@
+#include <iostream>
+
+class Shape{
+public:
+    virtual double area() const=0; // Значит объявление будет только у наследников
+};
+
+class Triange:public Shape{
+public:
+    double area() const override{
+        return 0.0;
+    }
+};
+
+void on_abstract_class(){
+    Triange tr;
+    Shape& s =tr;
+    std::cout<<s.area(); //вызовится для квадрата
+}
