128103e71e98327c34bf5975d1b0817938232631
Robbivan <96996213+robbivan@users.noreply.github.com>
2023-02-08 02:01:16 +0300

    Add new templates

--- multiple_inheritance.h
+++ multiple_inheritance.h
@@ -1,29 +1,33 @@
 #include <iostream>
-struct GrannyB{
+
+struct GrannyB {
     int f = 0;
 };
 
-struct Mother:public GrannyB{
-    int m=1;
+struct Mother : public GrannyB {
+    int m = 1;
     //    int a;
 };
 
-struct Father:public GrannyB{
-    int f=2;
+struct Father : public GrannyB {
+    int f = 2;
     //    int a;
 };
 
-struct Son:public Mother, public Father{
+struct Son : public Mother, public Father {
     int s = 3;
 };
 
-void on_miltiple_inheritance(){
+
+
+
+void on_miltiple_inheritance() {
     Son s;
-    std::cout<<'\n'<<&s<<'\n';
+    std::cout << '\n' << &s << '\n';
     Father *pf = &s;
-    std::cout<<pf<<'\n'; // разница в 4 байта, так как
+    std::cout << pf << '\n'; // разница в 4 байта, так как
     //    [mother 4] [father 4] [son 4], т. е. указатель как бы сдвинулся (неявно кастанулся)
-    std::cout<< static_cast<Son*>(pf); // так можно вернуться
+    std::cout << static_cast<Son *>(pf); // так можно вернуться
     //    std::cout<<s.Mother::a;
 
     // Diamond problem или ромбовидное наследование
@@ -31,11 +35,11 @@ void on_miltiple_inheritance(){
     // [g] [m] [g] [f] [s]
     // чтобы обращаться, нужно явно указывать
 
-    std::cout<<s.Mother::f;
+    std::cout << s.Mother::f;
 
     Mother m;
     //    dynamic_cast<Son&>(m); каст вниз - нельзя
-    std::cout<<(typeid(m)==typeid(s));
+    std::cout << (typeid(m) == typeid(s));
 
 
 }
--- templates_intro.h
+++ templates_intro.h
@@ -1,12 +1,47 @@
-template <typename T>
-const T& maximum(const T& a, const T& b){
+#include <iostream>
+#include <map>
+//template <typename T>
+//const T& maximum(const T& a, const T& b){
+//    return a>b?a:b;
+//}
+
+template <typename T, typename U>
+T maximum(const T& a, const U& b){
     return a>b?a:b;
 }
+
+// Шаблонный класс
+template <typename T> //template <class T> тоже самое
+class vector{
+    // Шаблонный метод
+public:
+    template<typename U>
+    void push_back(const U&){
+    }
+};
+
+// Равносильно вне класса:
+//template <typename T>
+//template <typename U>
+//void vector<T>::push_back(const U& x) {};
+
+// Шаблонные псевдонимы
+// C++11
+template <typename T>
+using mymap = std::map<T,T>;
+
+
+// Шаблонные переменные
+// С++14
+template <typename T>
+const T pi = 3.14;
+
+
 
 void on_template_intro() {
     maximum(1,2);
     maximum<int>(1,2.0); //Приведение к общему int
-
+    mymap<int> m;
     int a=0;
 //    ++a;
 }
