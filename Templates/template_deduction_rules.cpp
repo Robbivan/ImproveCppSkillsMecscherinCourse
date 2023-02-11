f4c14afa8103bfb1f1f0a629237941a332459d95
Robbivan <96996213+robbivan@users.noreply.github.com>
2023-02-11 20:57:40 +0300

    Add template deduction rules and ref wrapper

--- Templates/template_deduction_rules.cpp
+++ Templates/template_deduction_rules.cpp
@@ -1,3 +1,25 @@
-//
-// Created by Ivan on 11.02.2023.
-//
+#include <iostream>
+#include <functional>
+template <typename T>
+class C{
+    C()=delete;
+};
+
+
+template <typename T>
+void f(T x){
+    C<T> c; // трюк Скотта Мейерса
+    // здесь специально ошибка, чтобы понять, что на этапе компиляции подставил компилятор внутрь с
+}
+
+
+int main(){
+    int x =0;
+
+    int& y = x;
+
+    const int& z=x;
+
+    f(std::ref(x)); //references wrapper (отличие от обычной ссылки - если ее переприсваивать, то слетит)
+    // легковесная ссылка std::cref константная ссылка
+}
