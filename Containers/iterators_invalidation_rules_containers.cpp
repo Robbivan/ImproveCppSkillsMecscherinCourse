f2b3e3375501b720fa52bb0bc49caa75f895852a
Robbivan <96996213+robbivan@users.noreply.github.com>
2023-03-11 02:19:40 +0300

    Added iterators invalidation example

--- Containers/iterators_invalidation_rules_containers.cpp
+++ Containers/iterators_invalidation_rules_containers.cpp
@@ -1,3 +1,20 @@
-//
-// Created by Ivan on 11.03.2023.
-//
+#include <iostream>
+#include <vector>
+
+
+
+int main(){
+    std::vector<int> v;
+    v.push_back(1);
+
+    std::vector<int>::iterator it =v.begin();
+    int *p = &v.front();
+    int &r = v.front();
+    // все 3 варианта приведут к инвалидации итераторов при
+    v.push_back(3);
+    // после этого произошло "перекладывание" в новый динамический массив с каким-то capacity
+    // а все что указывало на элемент в изначальном векторе стало UB
+
+
+
+}
