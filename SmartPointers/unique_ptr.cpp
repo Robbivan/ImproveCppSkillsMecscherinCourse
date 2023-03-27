9f4e4cf84acea48fd512b75668a13141228f313d
Robbivan <96996213+robbivan@users.noreply.github.com>
2023-03-28 02:20:38 +0300

    Added unique_ptr

--- SmartPointers/unique_ptr.cpp
+++ SmartPointers/unique_ptr.cpp
@@ -1,3 +1,36 @@
-//
-// Created by Ivan on 26.03.2023.
-//
+#include <iostream>
+#include <memory>
+#include <vector>
+// умные указатели позволяют освобождать автоматически динамически
+// выделенный ресурс
+
+
+void f_f(){
+    std::unique_ptr<int> p(new int(5));
+    // unique_ptr скопировать нельзя, не ведет подсчет ссылок
+    // auto pp = p; // Нельзя, CE
+    // создавать 2 указателя unique_ptr на одно и тоже = утечка
+
+    // аналогично и с shared_ptr, можно присвоить это ок, так как подсчет ссылок будет в одном месте,
+    // но 2 разных подсчета на двух разных shared_ptr = утечка
+
+    std::vector<std::unique_ptr<int>> v;
+    for (int i = 0;i < 10; ++i){
+        v.emplace_back(new int(i)); // все хорошо, так как move(forward) разрешен
+    }
+    for (size_t i = 0; i < 10; ++i){
+        std::cout<<*v[i];
+    }
+}
+
+
+void f(){
+    std::shared_ptr<int> p(new int(5));
+    auto pp = p;
+}
+
+
+int main(){
+    auto some_val = new int(5);
+    std::cout<<*some_val;
+}
