9a6eeb9da62abeadd283fab8631aa40953ffe114
Robbivan <96996213+robbivan@users.noreply.github.com>
2023-04-04 15:46:45 +0300

    Added a implementation of shared_ptr

--- SmartPointers/shared_ptr.cpp
+++ SmartPointers/shared_ptr.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 #include <memory>
 
+
+template <typename T>
+class shared_ptr{
+    T*ptr = nullptr;
+    size_t* count = nullptr;
+
+public:
+    explicit shared_ptr(T* ptr_):ptr(ptr_),count(new size_t(1)){}
+
+    size_t use_count(/*...*/) const noexcept{}
+
+    T* get() const noexcept{}
+
+    ~shared_ptr(){
+        if(!count) return;
+        --*count;
+        if(!*count){
+            delete ptr;
+            delete count;
+        }
+    }
+};
 int main(){
 
 }
